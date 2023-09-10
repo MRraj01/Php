dc1dc33efd5937f19ebcbaee5cfc40a54c2bd4ec
unknown <karthickraj.20cse@sonatech.ac.in>
2023-09-10 17:15:41 +0530

    updating the img for img top

--- _template/_Game.php
+++ _template/_Game.php
@@ -4,14 +4,15 @@
 		<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 			<div class="col">
 				<div class="card shadow-sm">
-					<svg class="bd-placeholder-img card-img-top" width="100%" height="225"
-						xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
-						preserveAspectRatio="xMidYMid slice" focusable="false">
-						<title>Placeholder</title>
-						<rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"
-							dy=".3em">Thumbnail</text>
-					</svg>
+					
+						<img  src="assets\cropped-1500-900-1328866.png" class="card-img-top" width="100%" height="225" ></img>
+			<!--
+				download image resol as 1500*900
+				then remove svg tags and add img src for the image file
 
+
+
+			 -->
 					<div class="card-body">
 						<p class="card-text">This is a wider card with supporting text below as a natural
 							lead-in to additional content. This content is a little bit longer.</p>
