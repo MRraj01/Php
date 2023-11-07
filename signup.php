390a2c0627534b1e0f99777a1493652f5238daf0
MRbeast032 <127408468+MRbeast032@users.noreply.github.com>
2023-11-07 23:41:58 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -5,19 +5,18 @@ include 'libs/load.php';
 
 ?>
 
-<?load_template('_head');?>
+<?php load_template('_head');?>
 
 <body>
-	<?load_template('_header');?>
-	</header>
+
 	<main>
 		<section class="py-5 text-center container">
 			<?php
   load_template('_signup'); ?>
 		</section>
 	</main>
-	<?load_template('_footer');?>
-	<script src="/app/assets/dist/js/bootstrap.bundle.min.js"></script>
+	
+	<script src="/Php/assets/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
